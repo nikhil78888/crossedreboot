8c11070a99fa0948cd1544677bf91d34af698196
Chetan <chetan@Chetans-MacBook-Pro.local>
2023-07-13 20:47:02 +0530

    prepare api for deployment

--- apps/api/src/lib/openai.ts
+++
@@ -1,8 +0,0 @@
-import { OpenAI } from "langchain/llms/openai";
-import { apiConfig } from "../api-config";
-
-export const openAILLM = new OpenAI({
-  modelName: "gpt-4-0613",
-  openAIApiKey: apiConfig.openAIApiKey,
-  temperature: 0.5,
-});
